15d189b1cf7862e1bb2e1141a8a6d210b38b1bdb
Željko Filipin <zeljko.filipin@gmail.com>
2022-12-20 15:15:42 +0100

    Add assertions

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -4,12 +4,17 @@ describe('Homepage', function () {
     await browser.url('./');
 
     // Get the title of the homepage, should be 'Conduit'
-    console.log(await browser.getTitle());
+    if (await browser.getTitle() !== 'Conduit') {
+      // throw an error explaining what went wrong
+      throw new Error('Title of the homepage should be "Conduit"');
+    }
 
     // Click the 'Sign in' navigation link
     await $('[href*=login]').click();
 
     // Get the URL of the sign in page. It should include 'login'
-    console.log(await browser.getUrl());
+    if (await browser.getUrl() !== 'http://localhost:8080/login') {
+      throw new Error('URL of "login" page should be correct');
+    }
   });
 });
