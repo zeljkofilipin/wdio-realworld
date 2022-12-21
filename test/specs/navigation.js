2d3928c2ed41186d9bd1328b8894d2bae278ccaa
Željko Filipin <zeljko.filipin@gmail.com>
2022-12-21 13:07:52 +0100

    Use nodejs assert library

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -1,20 +1,17 @@
+const assert = require('assert');
+
 describe('Homepage', function () {
   it('should load properly', async function () {
     // load the page
     await browser.url('./');
 
     // Get the title of the homepage, should be 'Conduit'
-    if (await browser.getTitle() !== 'Conduit') {
-      // throw an error explaining what went wrong
-      throw new Error('Title of the homepage should be "Conduit"');
-    }
+    assert.strictEqual(await browser.getTitle(), 'Conduit');
 
     // Click the 'Sign in' navigation link
     await $('[href*=login]').click();
 
     // Get the URL of the sign in page. It should include 'login'
-    if (await browser.getUrl() !== 'http://localhost:8080/login') {
-      throw new Error('URL of "login" page should be correct');
-    }
+    assert.strictEqual(await browser.getUrl(), 'http://localhost:8080/login');
   });
 });
