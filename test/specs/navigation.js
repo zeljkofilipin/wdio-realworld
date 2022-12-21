a54f82d826f9c79cec6c35941d66270475e5b73d
Željko Filipin <zeljko.filipin@gmail.com>
2022-12-21 13:24:47 +0100

    Use wdio expect library

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -1,17 +1,15 @@
-const assert = require('assert');
-
 describe('Homepage', function () {
   it('should load properly', async function () {
     // load the page
     await browser.url('./');
 
     // Get the title of the homepage, should be 'Conduit'
-    assert.strictEqual(await browser.getTitle(), 'Conduit');
+    await expect(browser).toHaveTitle('Conduit');
 
     // Click the 'Sign in' navigation link
     await $('[href*=login]').click();
 
     // Get the URL of the sign in page. It should include 'login'
-    assert.strictEqual(await browser.getUrl(), 'http://localhost:8080/login');
+    await expect(browser).toHaveUrl('http://localhost:8080/login');
   });
 });
