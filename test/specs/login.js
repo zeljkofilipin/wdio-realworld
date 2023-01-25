3a44b8db6fe3fee7f0749460ee78a2500576358e
Željko Filipin <zeljko.filipin@gmail.com>
2023-01-25 12:49:41 +0100

    login: write steps for the first test

--- test/specs/login.js
+++ test/specs/login.js
@@ -1,4 +1,12 @@
-// Login Page:
-// should let you log in
-// should error with a missing username
-// should error with a missing password
+describe('Login Page', function () {
+  it('should let you log in', function () {
+    // go to the login page
+    // enter a valid username in the "email" input
+    // enter a valid password in the "password" input
+    // click the 'Sign In' button
+    // assert that we're logged in
+  } )
+  // should error with a missing username
+  // should error with a missing password
+}
+)
