83c0cb1d58830bc1d21b95480766a9e6338479fd
Željko Filipin <zeljko.filipin@gmail.com>
2023-01-26 14:04:50 +0100

    login: move common code to a function

--- test/specs/login.js
+++ test/specs/login.js
@@ -1,25 +1,24 @@
+async function login(email, password) {
+  await $('input[type="email"]').setValue(email);
+  await $('input[type="password"]').setValue(password);
+  await $('button*=Sign in').click();
+}
+
 describe('Login Page', function () {
   beforeEach(async function () {
     await browser.url('./login');
   })
   it('should let you log in', async function () {
-    await $('input[type="email"]').setValue('demo@learnwebdriverio.com');
-    await $('input[type="password"]').setValue('wdiodemo');
-
-    const $signIn = $('button*=Sign in');
-    await $signIn.click();
-    await $signIn.waitForExist({ reverse: true });
-
+    await login('demo@learnwebdriverio.com', 'wdiodemo');
+    $('button*=Sign in').waitForExist({ reverse: true });
     await expect($('.error-messages li')).not.toBeExisting();
   } )
   it('should error with a missing username', async function () {
-    await $('input[type="password"]').setValue('wdiodemo');
-    await $('button*=Sign in').click();
+    await login('', 'wdiodemo');
     await expect($('.error-messages li')).toHaveText(`email can't be blank`);
   });
   it('should error with a missing password', async function () {
-    await $('input[type="email"]').setValue('demo@learnwebdriverio.com');
-    await $('button*=Sign in').click();
+    await login('demo@learnwebdriverio.com', '');
     await expect($('.error-messages li')).toHaveText(`password can't be blank`);
 });
 }
