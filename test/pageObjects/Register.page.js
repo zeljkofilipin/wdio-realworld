ef43fa5e1f96e2500d19d5eb61fb8f412d09d623
Željko Filipin <zeljko.filipin@gmail.com>
2023-01-27 13:46:39 +0100

    register: Register Page requires username

---
+++ test/pageObjects/Register.page.js
@@ -0,0 +1,15 @@
+class Register{
+  get email() { return $('input[placeholder="Email"]'); }
+  get error() { return $('.error-messages'); }
+  get password() { return $('input[placeholder="Password"]'); }
+  get signUp() { return $('.btn'); }
+  get username() { return $('input[placeholder="Username"]'); }
+
+  async register(username, email, password) {
+    await this.username.setValue(username);
+    await this.email.setValue(email);
+    await this.password.setValue(password);
+    await this.signUp.click();
+  }
+}
+module.exports = Register;
---
+++ test/specs/register.js
@@ -0,0 +1,14 @@
+const Register = require('../pageObjects/Register.page');
+const register = new Register();
+
+describe('Register Page', function () {
+  beforeEach(async function () {
+    await browser.url('./register');
+  })
+  it('requires username', async function () {
+    await register.register('', 'register@learnwebdriverio.com', 'password');
+    await browser.pause(3000)
+    await expect(register.error).toHaveText(`username can't be blank`);
+  } )
+}
+)
