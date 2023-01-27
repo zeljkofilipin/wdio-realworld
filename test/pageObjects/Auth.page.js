d9c5b720368dbb95dbaefad5afccb69ff014cff5
Željko Filipin <zeljko.filipin@gmail.com>
2023-01-27 13:01:33 +0100

    auth: move to a separate file

---
+++ test/pageObjects/Auth.page.js
@@ -0,0 +1,24 @@
+class Auth {
+  get $email () { return $('input[type="email"]'); }
+  get $password () { return $('input[type="password"]'); }
+  get $signIn () { return $('button*=Sign in'); }
+  get $errorMessages () { return $('.error-messages li'); }
+
+  async login(email, password) {
+    await this.$email.setValue(email);
+    await this.$password.setValue(password);
+    await this.$signIn.click();
+    browser.waitUntil(
+      () => {
+        const signInExists = this.$signIn.isExisting();
+        const errorExists = this.$errorMessages.isExisting();
+        return !signInExists || errorExists;
+      },
+      {
+        timoutMsg:
+          'The "Sign in" button still exists and an error never appeared'
+      }
+    );
+  }
+}
+module.exports = Auth;
--- test/specs/login.js
+++ test/specs/login.js
@@ -1,27 +1,4 @@
-class Auth {
-  get $email () { return $('input[type="email"]'); }
-  get $password () { return $('input[type="password"]'); }
-  get $signIn () { return $('button*=Sign in'); }
-  get $errorMessages () { return $('.error-messages li'); }
-
-  async login(email, password) {
-    await this.$email.setValue(email);
-    await this.$password.setValue(password);
-    await this.$signIn.click();
-    browser.waitUntil(
-      () => {
-        const signInExists = this.$signIn.isExisting();
-        const errorExists = this.$errorMessages.isExisting();
-        return !signInExists || errorExists;
-      },
-      {
-        timoutMsg:
-          'The "Sign in" button still exists and an error never appeared'
-      }
-    );
-  }
-}
-
+const Auth = require('../pageObjects/Auth.page');
 const auth = new Auth();
 
 describe('Login Page', function () {
