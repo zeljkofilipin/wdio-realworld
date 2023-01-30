9ff109b1dae8786c19754c2c6466bc721bd37a27
Željko Filipin <zeljko.filipin@gmail.com>
2023-01-30 13:44:21 +0100

    editor: should load properly

--- test/specs/editor.js
+++ test/specs/editor.js
@@ -1,11 +1,23 @@
 const Auth = require('../pageObjects/Auth.page');
+const Editor = require('../pageObjects/Editor.page');
+
 const auth = new Auth();
+const editor = new Editor();
+
 describe('Post Editor', function () {
-  it('should load page properly', function () {
-    // Load the login page
-    // Login with a valid user
-    // Load the Post Editor page
-    // Assert the URL is correct
-    // Assert the page fields are correct
+  before(async function () {
+    await browser.url('./login');
+    await auth.login('demo@learnwebdriverio.com', 'wdiodemo');
+  })
+  this.beforeEach(async function () {
+    await browser.url('./editor');
+  })
+  it('should load page properly', async function () {
+    await expect(browser).toHaveUrl('editor', { containing: true });
+    await expect(editor.$title).toBeExisting();
+    await expect(editor.$description).toBeExisting();
+    await expect(editor.$body).toBeExisting();
+    await expect(editor.$tags).toBeExisting();
+    await expect(editor.$publish).toBeExisting();
   });
 });
