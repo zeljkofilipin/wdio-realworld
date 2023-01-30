8e707a78b5d724b71bae71cd2d360cce262af5ed
Željko Filipin <zeljko.filipin@gmail.com>
2023-01-30 13:27:53 +0100

    editor: just comments

---
+++ test/specs/editor.js
@@ -0,0 +1,11 @@
+const Auth = require('../pageObjects/Auth.page');
+const auth = new Auth();
+describe('Post Editor', function () {
+  it('should load page properly', function () {
+    // Load the login page
+    // Login with a valid user
+    // Load the Post Editor page
+    // Assert the URL is correct
+    // Assert the page fields are correct
+  });
+});
