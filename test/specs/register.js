8dfffc452172dc4a36f6cd95acba335d45cd4e71
Željko Filipin <zeljko.filipin@gmail.com>
2023-02-08 13:39:00 +0100

    register: fix tests

--- test/specs/register.js
+++ test/specs/register.js
@@ -6,31 +6,31 @@ describe('Register Page', function () {
     await browser.url('./register');
   })
   it('requires username', async function () {
-    await register.register('', 'register@learnwebdriverio.com', 'password');
+    await register.register('', 'register10@learnwebdriverio.com', 'password');
     await expect(register.error).toHaveText(`username can't be blank`);
   } )
   it('requires email', async function () {
-    await register.register('register', '', 'password');
+    await register.register('register11', '', 'password');
     await expect(register.error).toHaveText(`email can't be blank`);
   } )
   it('requires password', async function () {
-    await register.register('register', 'register@learnwebdriverio.com', '');
+    await register.register('register12', 'register12@learnwebdriverio.com', '');
     await expect(register.error).toHaveText(`email can't be blank`);
   } )
   it('requires unique username', async function () {
-    await register.register('demo', 'register2@learnwebdriverio.com', 'password');
+    await register.register('demo', 'register13@learnwebdriverio.com', 'password');
     await expect(register.error).toHaveText(`username is already taken.`);
   } )
   it('requires valid email', async function () {
-    await register.register('register2', 'register2', 'password');
+    await register.register('register14', 'register14', 'password');
     await expect(register.error).toHaveText(`email is invalid`);
   } )
   it('requires unique email', async function () {
-    await register.register('register2', 'demo@learnwebdriverio.com', 'password');
+    await register.register('register15', 'demo@learnwebdriverio.com', 'password');
     await expect(register.error).toHaveText(`email is already taken.`);
   } )
   it('opens home page after registration', async function () {
-    await register.register('register3', 'register3@learnwebdriverio.com', 'password');
+    await register.register('register16', 'register16@learnwebdriverio.com', 'password');
     await expect(browser).toHaveUrl('https://demo.learnwebdriverio.com/')
   } )
 }
