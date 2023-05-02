8c39e611eb1b1de9da615cc485eda4e9ef77f4ff
Željko Filipin <zeljko.filipin@gmail.com>
2023-05-02 12:13:45 +0200

    spec: tag

---
+++ test/specs/tag.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { user1 } = require( '../fixtures/users' );
+const Tag = require( '../pageObjects/Tag.page' );
+
+describe( 'Tag Feed', function () {
+	let articleDetails, tagName, tagPage, articleResponse;
+
+	before( async function () {
+		articleDetails = {
+			title: chance.sentence( { words: 3 } ),
+			description: chance.sentence( { words: 7 } ),
+			body: chance.paragraph( { sentences: 2 } ),
+			tagList: [ chance.word( { length: 30 } ) ]
+		};
+		tagName = articleDetails.tagList[ 0 ];
+
+		// create the article we need to get the specific tag
+		articleResponse = await global.api.createArticle( user1, articleDetails );
+		tagPage = new Tag( tagName );
+
+		// load the page
+		await tagPage.load();
+	} );
+
+	it( 'should have tag tab', async function () {
+		// check that we're on the tag tab
+		expect( await tagPage.activeFeedTabText[ 0 ] ).toEqual( tagName, {
+			trim: true
+		} );
+	} );
+
+	it( 'should load only articles for that tag', async function () {
+		await expect( tagPage.currentFeed.$$articles ).toBeElementsArrayOfSize( 1 );
+	} );
+
+	it( 'should load correct article preview details', async function () {
+		const firstArticleDetails = await tagPage.currentFeed.articles[ 0 ].getDetails();
+		expect( firstArticleDetails ).toMatchObject( {
+			title: articleDetails.title,
+			description: articleDetails.description
+		} );
+	} );
+} );
--- utils/Api.js
+++ utils/Api.js
@@ -1,26 +1,34 @@
 'use strict';
 
-const got = require( 'got' );
+const axios = require( 'axios' );
 
 class Api {
-	constructor( prefixUrl ) {
-		this.client = got.extend( {
-			prefixUrl,
-			responseType: 'json'
+	constructor( baseURL ) {
+
+		this.api = axios.create( {
+			baseURL,
+			Accept: 'application/json, text/plain, */*',
+			'Content-Type': 'application/json'
 		} );
 	}
-
-	async getAuthToken( { email, password } ) {
-		try {
-			const { body } = await this.client.post( 'users/login', {
-				json: { user: { email, password } }
-			} );
-
-			return body.user.token;
-		} catch ( err ) {
-			console.log( err );
-		}
+	getAuthToken( { email, password } ) {
+		return this.api
+			.post( '/users/login', { user: { email, password } } )
+			.then( ( res ) => res.data.user.token );
+	}
+	async createArticle( user, details ) {
+		const token = await this.getAuthToken( user );
+		const response = await this.api.post( 'articles',
+			{
+				article: details
+			},
+			{
+				headers: {
+					Authorization: `Token ${token}`
+				}
+			}
+		);
+		return response.data.article;
 	}
 }
-
 module.exports = Api;
